50f56ce52e3d90cca15d6ee2e535fc408d1afbd3
tjphillips08 <tjphillips08@icloud.com>
2022-10-21 12:35:44 -0500

    Updates

---
+++ precache-manifest.3f4cbcc940eb2919e174c1388de13a75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdd1a4c02d7f1142a8c9bdf340983a9",
+    "url": "/reactjs-frontend-portfolio/index.html"
+  },
+  {
+    "revision": "8ba1859021ec664bbba2",
+    "url": "/reactjs-frontend-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a41c83ab3dd9094e7594",
+    "url": "/reactjs-frontend-portfolio/static/css/main.bfe577ab.chunk.css"
+  },
+  {
+    "revision": "8ba1859021ec664bbba2",
+    "url": "/reactjs-frontend-portfolio/static/js/2.72dd7e96.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/reactjs-frontend-portfolio/static/js/2.72dd7e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41c83ab3dd9094e7594",
+    "url": "/reactjs-frontend-portfolio/static/js/main.82e72a91.chunk.js"
+  },
+  {
+    "revision": "928f552712b360d69017",
+    "url": "/reactjs-frontend-portfolio/static/js/runtime-main.e7896b7e.js"
+  }
+]);
